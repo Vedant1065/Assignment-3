443d83ba1ab1730f0203f6df13ee16278a756861
Vedant1065 <47012073+Vedant1065@users.noreply.github.com>
2019-02-01 11:46:25 +0530

    Add files via upload

---
+++ DDL.sql
@@ -0,0 +1,31 @@
+*/
+Created: 1/30/2019
+Modified: 1/30/2019
+Model: Microsoft SQL Server 2017
+Database: MS SQL Server 2017
+*/
+
+
+-- Create tables section -------------------------------------------------
+
+-- Table Male_Employees
+
+CREATE TABLE [Male_Emp]
+(
+ [First_Name] Nvarchar(100) NULL,
+ [Last_Name] Nvarchar(100) NULL,
+ [Domain] Nvarchar(100) NULL,
+ [Login] Nvarchar(100) NULL
+)
+go
+
+-- Table Female_Employees
+
+CREATE TABLE [Female_Emp]
+(
+ [First_Name] Nvarchar(100) NULL,
+ [Last_Name] Nvarchar(100) NULL,
+ [Domain] Nvarchar(100) NULL,
+ [Login] Nvarchar(100) NULL
+)
+go
